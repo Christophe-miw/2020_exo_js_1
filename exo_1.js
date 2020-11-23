ebeab8d4a0c5715c79f4d440b6702aea3411f387
florent <florent.luminet@gmail.com>
2020-11-23 11:15:58 +0100

    exo filter + reduce

--- exo_1.js
+++ exo_1.js
@@ -18,8 +18,15 @@ let taches = [
 
 
 
-console.log(taches.map(object => object.nom));
+//console.log(taches.map(object => object.nom));
 
-console.log(taches.filter(object => object.priorite === 1));
+//console.log(taches.filter(object => object.priorite === 1));
 
-console.log(taches.reduce((addition, tache) => addition + tache.duree,0 ));
+//console.log(taches.reduce((addition, tache) => addition + tache.duree, 0));
+
+let total = []
+taches.reduce((x, object) => {
+	if(object.priorite === 1) total.push(object)
+}, total)
+
+console.log(total)
