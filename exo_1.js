dda72bba098511210d1ba6aa6c48ed252d1c5b9a
Yohan Salamone <yohan.salamone@outlook.com>
2020-11-23 11:12:38 +0100

    exo 2b avec reduce au lieu de filter

--- exo_1.js
+++ exo_1.js
@@ -22,6 +22,14 @@ console.log(taches.map(tache => tache.nom));
 console.log('\n2.');
 console.log(taches.filter(tache => tache.priorite === 1));
 
+console.log('\n2b.');
+let prio1 = (res, tache) => {
+	if(tache.priorite === 1)
+		res.push(tache);
+	return res;
+};
+console.log(taches.reduce(prio1, []));
+
 console.log('\n3.');
 let sommeDurees = (somme, tache) => somme + tache.duree;
 console.log(taches.reduce(sommeDurees, 0));
