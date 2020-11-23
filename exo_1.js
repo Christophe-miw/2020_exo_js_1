58bd673cda0b06c18dc45f1887e47742f6ef6a53
Yohan Salamone <yohan.salamone@outlook.com>
2020-11-23 10:17:14 +0100

    exercices faits

--- exo_1.js
+++ exo_1.js
@@ -15,3 +15,16 @@ let taches = [
 		priorite: 1,
 	},
 ];
+
+console.log('1.');
+for(let [index, tache] of Object.entries(taches)) {
+	console.log(tache.nom);
+}
+
+console.log('\n2.');
+let resQ2 = taches.filter(tache => tache.priorite === 1);
+console.log(resQ2);
+
+console.log('\n3.');
+let sommeDurees = (somme, tache) => somme + tache.duree;
+console.log(taches.reduce(sommeDurees, 0));
