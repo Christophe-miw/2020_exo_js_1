bb972f72645e26c1d7174eb05ad829ef6eaa44a4
CesarWatrin <cesar.watrin@hotmail.com>
2020-11-23 10:14:43 +0100

    exo js

--- exo_1.js
+++ exo_1.js
@@ -1,17 +1,39 @@
 let taches = [
-	{
-		nom: 'Travailler efficacement',
-		duree: 180,
-		priorite: 3,
-	},
-	{
-		nom: 'Boire un café',
-		duree: 20,
-		priorite: 1,
-	},
-	{
-		nom: 'Discuter avec ses collèges',
-		duree: 120,
-		priorite: 1,
-	},
+   {
+      nom: 'Travailler efficacement',
+      duree: 180,
+      priorite: 3,
+   },
+   {
+      nom: 'Boire un café',
+      duree: 20,
+      priorite: 1,
+   },
+   {
+      nom: 'Discuter avec ses collèges',
+      duree: 120,
+      priorite: 1,
+   },
 ];
+
+var noms = [];
+for (var i = 0; i < taches.length; i++) {
+   noms.push(taches[i].nom);
+}
+var noms = [];
+for (var i = 0; i < taches.length; i++) {
+   noms.push(taches[i].nom);
+}
+var priorites = [];
+for (var i = 0; i < taches.length; i++) {
+   if (taches[i].priorite == 1) {
+      priorites.push(taches[i].priorite);
+   }
+}
+var totalDur = 0;
+for (var i = 0; i < taches.length; i++) {
+   totalDur += taches[i].duree;
+}
+console.log(noms);
+console.log(priorites);
+console.log(totalDur);
