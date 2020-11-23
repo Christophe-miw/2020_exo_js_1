36afe69f187539c3d22cc494459f49cb19ae9179
julien leger <julienleger2000@yahoo.fr>
2020-11-23 10:14:58 +0100

    c

--- exo_1.js
+++ exo_1.js
@@ -1,17 +1,32 @@
-let taches = [
-	{
-		nom: 'Travailler efficacement',
-		duree: 180,
-		priorite: 3,
-	},
-	{
-		nom: 'Boire un café',
-		duree: 20,
-		priorite: 1,
-	},
-	{
-		nom: 'Discuter avec ses collèges',
-		duree: 120,
-		priorite: 1,
-	},
+let taches = [{
+        nom: 'Travailler efficacement',
+        duree: 180,
+        priorite: 3,
+    },
+    {
+        nom: 'Boire un café',
+        duree: 20,
+        priorite: 1,
+    },
+    {
+        nom: 'Discuter avec ses collèges',
+        duree: 120,
+        priorite: 1,
+    },
 ];
+
+
+let Noms = []
+let priorite = []
+taches.forEach((element) => Noms.push(element.nom))
+taches.forEach((element) => {
+    if (element.priorite == 1)
+        priorite.push(element.priorite)
+
+
+})
+let resultat = 0
+taches.forEach((element) => resultat += element.duree)
+console.log(resultat)
+console.log(Noms)
+console.log(priorite)
