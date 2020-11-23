0522d372d617dfb749f9f035a22dc41de8c1049f
JLP0578 <54902345+JLP0578@users.noreply.github.com>
2020-11-23 10:33:12 +0100

    V_Final

--- exo_1.js
+++ exo_1.js
@@ -1,3 +1,4 @@
+//J_Le_Pivert
 let taches = [
 	{
 		nom: 'Travailler efficacement',
@@ -15,3 +16,15 @@ let taches = [
 		priorite: 1,
 	},
 ];
+
+// array nom
+const map1 = taches.map(itemTableauTache => itemTableauTache);
+console.log(map1);
+
+// array priorites = 1
+const result = taches.filter(priorite => priorite.priorite == 1);
+console.log(result);
+
+// durée total
+let sommeDuree = taches.reduce((accumulateur, valeurCourante) => accumulateur + valeurCourante.duree,0);
+console.log(sommeDuree);
