e7139d57fd801822213d2a82f455456cd960b311
Claire-Sabatier <claire.sabatier@etu.umontpellier.fr>
2020-11-23 11:04:57 +0100

    exercices reduce

--- exo_1.js
+++ exo_1.js
@@ -16,10 +16,35 @@ let taches = [
 	},
 ];
 let somme = 0; 
-for( let i = 0; i < taches.length; i++){
+
+taches.map(obj => {
+	console.log(obj.nom);
+});
+
+taches.filter(obj => {
+	console.log(obj.priorite);
+});
+
+
+
+let duree = taches.map(obj => obj.duree).reduce((total, duree)=>{
+ return (total || 0) + parseInt(duree);
+}, 0);
+console.log(duree);
+
+// partie filter en utilisant reduce  
+
+let priorites = taches.reduce((prev, curr)=>{
+	return [prev, curr.priorite];
+}, 0)
+console.log(priorites.join(','));
+
+
+
+/*for( let i = 0; i < taches.length; i++){
 	console.log(taches[i].nom);
 	console.log(taches[i].priorite); 
 	
 	somme += taches[i].duree; 
 }
-console.log("total des durees : "+ somme);
+console.log("total des durees : "+ somme);*/
