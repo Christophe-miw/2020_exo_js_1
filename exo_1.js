3e1274b2c89051aa4ae9367753855c75a19a2904
COULON <maxime.coulon06@gmail.com>
2020-11-23 11:17:48 +0100

    ajout correction

--- exo_1.js
+++ exo_1.js
@@ -16,7 +16,7 @@ let taches = [
 	},
 ];
 
-// Array des nom : (map)
+// Array des nom :
 console.log("Array des noms : ")
 
 for (let property in taches){
@@ -24,6 +24,12 @@ for (let property in taches){
 	console.log(`${taches[property].nom}`)
 }
 
+
+// Autre moyen : (map)
+
+const Tnoms = taches.map((taches)=>taches.nom);
+console.log('array des noms : ');
+console.log(Tnoms);
 
 
 // Affichages des taches avec priorité = 1
@@ -35,18 +41,45 @@ console.log(result)
 
 
 
+/*		AVEC REDUCE TEST
+const ResultReduce = taches.reduce((accumulator, taches) =>{
+	let tab = accumulator.push[taches.nom];
+	return tab;
+}, []);
+
+
+console.log(ResultReduce)*/
+
+
 // Durée total :
 console.log("---------------------")
 console.log("durée total : ")
+
 const reducer = (accmulator, currentValue) => accmulator + currentValue;
+
 
 for (let property in taches){
 
 	const resultat =[`${taches[property].duree}`].reduce(reducer)
 
+
 	console.log(resultat)
 
 }
+
+// Correction :
+
+console.log(' Correction duree total : ')
+
+let init = 0;
+const Duree = taches.reduce((total, taches) => {
+	return total + taches.duree;
+},init);
+
+console.log(Duree)
+
+
+
 
 
 
